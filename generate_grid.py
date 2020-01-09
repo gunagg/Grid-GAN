1855094a1c8740ffedeb9e371b6f4ea935c836ba
Gunjan Aggarwal <gunjanagg1005@gmail.com>
2020-01-09 09:03:09 -0800

    Add files via upload

--- generate_grid.py
+++ generate_grid.py
@@ -5,6 +5,52 @@ def save_image(img, fname) :
 	img = Image.fromarray(img.astype(np.uint8))
 	img.save(fname)
 
+def remove_val(indices, val) :
+	if type(val) is list :
+		for element in val :
+			print(element)
+			indices.pop(indices.index(element))
+	else :
+		indices.pop(indices.index(val))
+	return indices
+
+def get_start(indices) :
+	pos = np.random.choice(indices, size = 1)[0]
+	pos = 5
+	i, j = int(pos/grid_size), pos%grid_size
+	img[i*step_h:(i+1)*step_h, j*step_w:(j+1)*step_w] = start_color
+	grid[i,j] = 1
+	return remove_val(indices, pos), pos
+
+def get_obstacles(indices) :
+	obstacles = np.random.randint(min_total_obstacles, max_total_obstacles)
+	positions = list(np.random.choice(indices, size = obstacles, replace = False))
+	for pos in positions :
+		i, j = int(pos/grid_size), pos%grid_size
+		img[i*step_h:(i+1)*step_h, j*step_w:(j+1)*step_w] = block_color
+		grid[i,j] = 1
+	return remove_val(indices, positions)
+
+def get_end(indices, start_pos) :
+	pos = np.random.choice(indices, size = 1)[0]
+	pos = 23
+	i, j = int(pos/grid_size), pos%grid_size
+	img[i*step_h:(i+1)*step_h, j*step_w:(j+1)*step_w] = end_color
+	grid[i,j] = 1
+	paths = [19, 20, 21, 22]
+	for path in paths :
+		i, j = int(path/grid_size), path%grid_size
+		img[i*step_h:(i+1)*step_h, j*step_w:(j+1)*step_w] = path_color
+	return remove_val(indices, pos)
+
+def generate(distance) :
+	indices = list(np.arange(grid_size*grid_size))
+	indices, start_pos = get_start(indices)
+	indices = get_obstacles(indices)
+	indices = get_end(indices, start_pos)
+
+
+
 def render(grid, img_size = 28) :
 	h,w = grid.shape
 	step_h = int(img_size/h)
@@ -27,16 +73,20 @@ def render(grid, img_size = 28) :
 
 	return img
 
-grid_size = 7
+img_size = 28
+grid_size = 14
+max_total_obstacles = 40
+min_total_obstacles = 20
 
+step_h = int(img_size/grid_size)
+step_w = int(img_size/grid_size)
 grid = np.zeros([grid_size, grid_size])
+img = np.ones([img_size, img_size])*255.0
+start_color = 100
+end_color = 200
+block_color = 0
+path_color = 150
 
-grid[0][2] = 1
-grid[2][1] = 1
-grid[2][2] = 1
-grid[0][1] = 2
-grid[1][3] = 3
-
-img = render(grid)
+generate(4)
 
 save_image(img, "grid.png")
